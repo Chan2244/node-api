8cf0a789d820c9ecd3deff8f219ff49281b6952a
Chan2244 <booth.chandler@yahoo.com>
2025-12-02 16:27:36 -0700

    update

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const routes = require("express").Router();
+const myController = require("../controllers");
+
+routes.get("/", myController.awesomeFunction);
+routes.get("/ttech", myController.tooeleTechFunction);
+
+module.exports = routes;
